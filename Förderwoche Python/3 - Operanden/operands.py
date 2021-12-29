39e6801b2d4126423bcca3337710dcb7f687fd23
dmnktrb <dominik.trebo@gmail.com>
2021-12-29 11:30:01 +0100

    Adding/Editing chapter 1, 2, 3

---
+++ Förderwoche Python/3 - Operanden/operands.py
@@ -0,0 +1,13 @@
+#Addition
+add = 2+3
+#Subtraktion
+sub = 3-2
+#Multiplikation
+multi = 2*3
+#Division
+div = 3/2
+
+print("Addition: " + str(add))
+print("Subtraktion: " + str(sub))
+print("Multiplikation: " + str(multi))
+print("Division: " + str(div))
--- Förderwoche Python/print.py
+++
@@ -1,7 +0,0 @@
-#Mit dem Print-Befehl kann man Werte im Terminal ausgeben lassen
-
-#Wichtig: String-Werte immer in "   " setzen
-print("Hallo Welt") #print(Hallo Welt) --> würde Fehler ausgeben
-
-print(4)
-
