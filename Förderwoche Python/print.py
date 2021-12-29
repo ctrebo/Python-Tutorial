91fc773426c40df6f8ea989f61c95f6903148a85
dmnktrb <dominik.trebo@gmail.com>
2021-12-29 10:42:56 +0100

    Adding existing files to the repo

---
+++ Förderwoche Python/print.py
@@ -0,0 +1,7 @@
+#Mit dem Print-Befehl kann man Werte im Terminal ausgeben lassen
+
+#Wichtig: String-Werte immer in "   " setzen
+print("Hallo Welt") #print(Hallo Welt) --> würde Fehler ausgeben
+
+print(4)
+
